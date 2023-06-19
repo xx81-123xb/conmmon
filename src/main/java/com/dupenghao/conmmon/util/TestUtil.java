d65e2db2920a0ecd6a97e588d6dd291dfc23ee48
dupenghao <123456>
2023-06-19 11:30:36 +0800

    1.添加自动推送gitee插件,在执行deploy后自动推送本地仓库的依赖到gitee

---
+++ src/main/java/com/dupenghao/conmmon/util/TestUtil.java
@@ -0,0 +1,12 @@
+package com.dupenghao.conmmon.util;
+
+/**
+ * Created by 杜鹏豪 on 2023/6/19.
+ */
+public class TestUtil {
+
+    public static void sayHello(){
+        System.out.println("hello");
+    }
+
+}
